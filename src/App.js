caa965376cc995b966b9c3ed4300de5942bd7922
DESKTOP-UQONBL6\goma <soreiyu52@gmail.com>
2020-11-20 23:40:42 +0900

    aws amplify test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import Amplify, {API, graphqlOperation} from 'aws-amplify'
+import {listTodos} from './graphql/queries'
+import awsExports from "./aws-exports";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+Amplify.configure(awsExports);
+
+const App = () => {
+    const [todos, setTodos] = useState([])
+
+    useEffect(() => {
+        fetchTodos()
+    }, [])
+
+    async function fetchTodos() {
+        try {
+            const todoData = await API.graphql(graphqlOperation(listTodos))
+            const todos = todoData.data.listTodos.items
+            setTodos(todos)
+        } catch (err) {
+            console.log('error fetching todos')
+        }
+    }
+
+    return (
+        <div className="App">
+            <h1>hello yusuke</h1>
+            {
+                todos.map((todo, index) => (
+                    <div key={todo.id ? todo.id : index} >
+                        <p >{todo.name}</p>
+                        <p >{todo.description}</p>
+                    </div>
+                ))
+            }
+        </div>
+    );
 }
 
 export default App;
